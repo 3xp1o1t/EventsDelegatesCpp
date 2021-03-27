29f854eea47ac55c12d3b348fbba2352c14e9339
3xp1o1t <jes.monti.503@gmail.com>
2021-03-26 21:21:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# EventsDelegatesCpp
+
+Eventos y Delegados con c++
+Controlar delegados con/sin parametros y 
+broadcasting de delegados.
+Crear eventos y sobrescribirlos.
+
+Developed with Unreal Engine 4
---
+++ Source/EventsDelegatesCpp/EventsDelegatesCpp.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EventsDelegatesCpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EventsDelegatesCpp, "EventsDelegatesCpp" );
